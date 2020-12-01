fc2aab47a15d33f5e84d6d2c55c6e1a53c7eee50
Afonso -aptsharp <aff-on@hotmail.com>
2020-12-01 14:42:05 -0300

    Falha de projeto de teste

---
+++ ProjetoTestes/CategoriasControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoTestes
+{
+    class CategoriasControllerTest
+    {
+
+        private readonly Mock<DbSet<Categoria>> .mockSet;
+
+
+    }
+}
